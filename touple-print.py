60d86560a6e8ffbe05399a506ac8e5180300bb20
satyaswamy <satyanarayanswamy@gmail.com>
2018-02-21 16:30:53 +0530

    adding all modified files

---
+++ touple-print.py
@@ -0,0 +1,10 @@
+"""
+Write a pythone script to print elements of the touple 
+"""
+
+file=("f1","f2","f3")
+count=1
+for i in file:
+	print (count,".",i)
+	count=+1
+
--- touple.py
+++ touple.py
@@ -7,4 +7,10 @@ while (i<5):
 	Tdata=input("Enter touple data:")
 	T.append(Tdata)
 	i+=1
-print (T)
+print ("The entered data in the touple is:----->",T)
+
+print("<-----------------The touple in numbered list:---------------->")
+count=1
+for i in T:
+	print(count,".",i)
+	count=+1
